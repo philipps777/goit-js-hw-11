d4e6382398423188560e62c8944d2a46e3ccdce8
philips777 <mymailforall@ukr.net>
2023-07-23 18:52:58 +0300

    change

--- src/index.js
+++ src/index.js
@@ -121,7 +121,7 @@ function scrolTop() {
 }
 
 async function handlerPagination(entries, observer) {
-  for (const entry of entries) {
+  for (let entry of entries) {
     if (entry.isIntersecting) {
       try {
         page += 1;
